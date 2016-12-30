82619fc00630c172ac798bb55be750d0a05f53fc
Niels Krijger <niels@kryger.nl>
2016-12-30 18:15:09 +0100

    Minor code style change

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,6 +10,7 @@ import del from 'del';
 const paths = {
   src: 'src',
   dest: 'lib',
+  lint: ['gulpfile.babel.js', 'src/**/*.js', 'test/**/*.js'],
 };
 
 gulp.task('clean', () => del(`${paths.dest}/*`));
@@ -23,13 +24,13 @@ gulp.task('build', ['clean'], () => {
 });
 
 gulp.task('lint', () => {
-  return gulp.src(['gulpfile.babel.js', 'src/**/*.js', 'test/**/*.js'])
+  return gulp.src(paths.lint)
     .pipe(eslint())
     .pipe(eslint.format());
 });
 
 gulp.task('watch', ['build', 'lint'], () => {
-  gulp.watch(['gulpfile.babel.js', 'src/**/*.js', 'test/**/*.js'], ['build', 'lint']);
+  gulp.watch(paths.lint, ['build', 'lint']);
 });
 
 gulp.task('default', ['build', 'lint']);
